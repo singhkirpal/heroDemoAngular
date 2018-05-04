2fb21861a8df38338157018cf84c120c40ad90be
singhkirpal <kirpal997@gmail.com>
2018-05-04 22:00:01 +0530

    changes to UI

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,15 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  hero :Hero={
-id:1,
-name:'windstorm'
-  };
+ selectedHero : Hero;
   
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
   }
 
 }
